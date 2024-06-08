75ca4b43d703d7a262eab72c7a122287851951cc
VictorMRD <victormrd2002@hotmail.com>
2024-06-08 13:42:55 -0700

    Fix RadioGroup component

--- src/components/ui/radio-group/RadioGroupItem.vue
+++ src/components/ui/radio-group/RadioGroupItem.vue
@@ -1,12 +1,8 @@
 <script setup>
-import { computed } from "vue";
-import {
-  RadioGroupIndicator,
-  RadioGroupItem,
-  useForwardProps,
-} from "radix-vue";
-import { CheckIcon } from "@radix-icons/vue";
-import { cn } from "@/lib/utils";
+import { computed } from 'vue'
+import { RadioGroupIndicator, RadioGroupItem, useForwardProps } from 'radix-vue'
+import { CheckIcon } from '@radix-icons/vue'
+import { cn } from '@/lib/utils'
 
 const props = defineProps({
   id: { type: String, required: false },
@@ -16,26 +12,26 @@ const props = defineProps({
   name: { type: String, required: false },
   asChild: { type: Boolean, required: false },
   as: { type: null, required: false },
-  class: { type: null, required: false },
-});
+  class: { type: [String, Object, Array], required: false }
+})
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props;
+  const { class: _, ...delegated } = props
+  return delegated
+})
 
-  return delegated;
-});
-
-const forwardedProps = useForwardProps(delegatedProps);
+const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
   <RadioGroupItem
     v-bind="forwardedProps"
     :class="
-cn(
-  'aspect-square h-4 w-4 rounded-full border border-slate-200 border-slate-900 text-slate-900 shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:border-slate-50 dark:text-slate-50 dark:focus-visible:ring-slate-300',
-  props.class
-)]"
+      cn(
+        'aspect-square h-4 w-4 rounded-full border border-slate-200 text-slate-900 shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:text-slate-50 dark:focus-visible:ring-slate-300',
+        props.class
+      )
+    "
   >
     <RadioGroupIndicator class="flex items-center justify-center">
       <CheckIcon class="h-3.5 w-3.5 fill-primary" />
