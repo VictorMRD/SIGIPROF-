8c040c6ea32dd43ab00b8dac840495a7e61864be
VictorMRD <victormrd2002@hotmail.com>
2024-06-10 21:01:13 -0700

    Add axios to project

---
+++ src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  withCredentials: true,
+  withXSRFToken: true,
+  baseURL: import.meta.env.VITE_API_URL
+})
+
+export default instance
